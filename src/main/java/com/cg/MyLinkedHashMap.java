dfe88537251f0681a2108be8f30401a77420dc4e
krove <krove@DESKTOP-Q5SED1R>
2020-10-13 18:07:15 +0530

    [Kashif]Added:Ability to find frequency of words in a paragraph

---
+++ src/main/java/com/cg/MyLinkedHashMap.java
@@ -0,0 +1,55 @@
+package com.cg;
+
+import java.util.ArrayList;
+
+public class MyLinkedHashMap<K, V> {
+	private final int arrayLength;
+	ArrayList<MyLinkedList<K>> myHashArray;
+
+	public MyLinkedHashMap() {
+		arrayLength = 10;
+		myHashArray = new ArrayList<>(arrayLength);
+		initializeHashArray();
+	}
+
+	public void initializeHashArray() {
+		for (int i = 0; i < arrayLength; i++)
+			this.myHashArray.add(null);
+	}
+
+	private int getArrayIndex(K key) {
+		int hashCode = Math.abs(key.hashCode());
+		int arrayIndex = hashCode % arrayLength;
+		return arrayIndex;
+	}
+
+	public V get(K key) {
+		int arrayIndex = this.getArrayIndex(key);
+		MyLinkedList<K> myLinkedList = this.myHashArray.get(arrayIndex);
+		if (myLinkedList == null)
+			return null;
+		MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) myLinkedList.search(key);
+		return (myMapNode == null) ? null : myMapNode.getValue();
+	}
+
+	public void add(K key, V value) {
+		int arrayIndex = this.getArrayIndex(key);
+		MyLinkedList<K> myLinkedList = this.myHashArray.get(arrayIndex);
+		if (myLinkedList == null) {
+			myLinkedList = new MyLinkedList<>();
+			this.myHashArray.set(arrayIndex, myLinkedList);
+		}
+		MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) myLinkedList.search(key);
+		if (myMapNode == null) {
+			myMapNode = new MyMapNode<>(key, value);
+			myLinkedList.append(myMapNode);
+		} else {
+			myMapNode.setValue(value);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "MyLinkedHashMap List{" + myHashArray + "}";
+	}
+}
